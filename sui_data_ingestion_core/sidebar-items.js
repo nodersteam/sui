f32a06378b81a875b6a2caf3e803ed4f04998a42
nodersteam <nodersteam@users.noreply.github.com>
2024-02-27 11:51:57 +0000

    deploy: bec7d1b02067af9f5a91841282436fc132e85f2b

---
+++ sui_data_ingestion_core/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["MAX_CHECKPOINTS_IN_PROGRESS"],"struct":["DataIngestionMetrics","FileProgressStore","IndexerExecutor","WorkerPool"],"trait":["ProgressStore","Worker"]};
